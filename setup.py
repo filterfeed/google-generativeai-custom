fd3f433cdea973e0db5807972bf2a3dfb8532419
CTO <cto@filterfeed.com.br>
2024-12-16 16:47:29 -0300

    add setuptools configuration for google-generativeai-custom package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="google-generativeai-custom",  # match the name you use after #egg=
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+)
