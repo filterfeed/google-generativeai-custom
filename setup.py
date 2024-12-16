b9e99e39ac7ec6d682d4e1a46499df787fdd308c
CTO <cto@filterfeed.com.br>
2024-12-16 16:56:21 -0300

    adds find packages

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="google-generativeai-custom",  # match the name you use after #egg=
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(include=['google', 'google.*']),
     install_requires=[],
 )
